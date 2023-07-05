a2c9bec19928ff3b640785d7e42f5fd779f2d6bb
Dean Alexander Santos Buño <94216668+deanalex-buno95@users.noreply.github.com>
2023-07-05 14:06:05 +0800

    After testing, find all documents within the fighters collection by using the `collection.find` in the `index` route in `app.py` and reflect the list of collections in `index.html`.

--- app.py
+++ app.py
@@ -5,22 +5,48 @@ RGG Fighters Wiki Server
 """
 
 
-from flask import Flask
+from flask import Flask, render_template
+
 import pymongo
+import urllib.parse
+
 
 # Main Flask app.
 app = Flask(__name__)
 
 # MongoDB cluster, database, and collections.
-cluster = pymongo.MongoClient("mongodb+srv://deanbuno323:<password>@cluster0.vslhfie.mongodb.net/?retryWrites=true&w=majority")
-db = cluster["rggfighterswiki"]
-collection_fighters = db["fighters"]
-collection_fighting_styles = db["fightingstyles"]
+
+
+"""
+Functions
+"""
+
+
+def connect_to_mongodb():
+    """
+    Connect to the NoSQL database from the cloud.
+    :return: database
+    """
+    client = pymongo.MongoClient(f'mongodb+srv://deanbuno323:fQXyf9O9awaHwT8S@cluster0.vslhfie.mongodb.net/?retryWrites=true&w=majority')
+    database = client.get_database('rggfighterswiki')
+    return database
+
+
+"""
+App Routes
+"""
 
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    """
+    Home page.
+    :return: Template of home page.
+    """
+    rggfighterswiki_database = connect_to_mongodb()
+    fighters_collection = rggfighterswiki_database.get_collection('fighters')
+    fighters = fighters_collection.find()
+    return render_template("index.html", fighters=fighters)
 
 
 if __name__ == '__main__':
